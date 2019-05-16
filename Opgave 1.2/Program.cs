a1df79ea70039633150b04df1ddd9d060a220b10
AspITSloth <fred4262@edu.campusvejle.dk>
2019-05-16 09:07:38 +0200

    Did some work

---
+++ Opgave 1.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words =
+            {
+                "herp",
+                "derp",
+                "ferp",
+                "lerp",
+                "nerp"
+            };
+
+            while (true)
+            {
+                Console.WriteLine(CapitalizedWords(words));
+                Console.WriteLine();
+
+                Console.ReadKey(true);
+            }
+        }
+
+        static string CapitalizedWords(string[] words)
+        {
+            string wordsToString = string.Empty;
+
+            foreach (var word in words)
+            {
+                wordsToString = wordsToString + word[0].ToString().ToUpper() + word.Substring(1) + " ";             
+            }
+
+            return wordsToString;
+        }
+    }
+}
---
+++ Opgave 1.3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave_1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ran = new Random();
+
+            int[] numbers = new int[10];
+
+            while (true)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    numbers[i] = ran.Next(1, 21);
+                }
+
+                foreach (var number in OrderNum(numbers))
+                {
+                    Console.WriteLine(number);
+                }
+                Console.ReadKey(true);
+
+            }
+        }
+
+        static List<int> OrderNum(int[] numbers)
+        {
+            var orderedNum = numbers.ToList().OrderBy(n => n) as List<int>;
+            return orderedNum;
+        }
+    }
+}
